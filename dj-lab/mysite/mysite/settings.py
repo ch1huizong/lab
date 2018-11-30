450ddd04c99b8508a4bb61e1cae8d07ccf27ae4b
root <root@k.k.lab>
2018-11-30 12:22:12 +0800

    polls_api测试完成,有bug不通过

--- dj-lab/mysite/mysite/settings.py
+++ dj-lab/mysite/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
     'django_extensions',
 
     'polls.apps.PollsConfig',
--- dj-lab/mysite/polls_api/urls.py
+++ dj-lab/mysite/polls_api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+
 from rest_framework.routers import DefaultRouter
+from rest_framework.documentation import include_docs_urls
+from rest_framework_swagger.views import get_swagger_view
 
 from .views import (
     PollViewSet, ChoiceList, CreateVote, 
@@ -9,13 +12,16 @@ from .views import (
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, base_name='polls')
+schema_view = get_swagger_view(title='Polls API')
 
 urlpatterns = [
     path('polls/<int:pk>/choices/', ChoiceList.as_view(), name='choice_list'),
     path('polls/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name='create_vote'),
-
     path('users/', UserCreate.as_view(), name='user_create'),
     path('login/', LoginView.as_view(), name='login'),
+
+    path('swagger-docs/', schema_view),
+    path('docs/', include_docs_urls(title='Polls API')),
 ]
 
 urlpatterns += router.urls
